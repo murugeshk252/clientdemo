3b4177d71e0181d2bfe82a3d7f3bdc95c306ee69
DESKTOP-12K0FS8\DELL <murugeshk252@gmail.com>
2020-07-09 11:39:52 +0530

    clientdemo commit

---
+++ src/app/files-service.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+// import { Http, Headers, ResponseContentType } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FilesServiceService {
+
+  // constructor(private http: Http) { }
+}
