7dcbe929a7098fac17d5e8393b326f178f33dcd3
murugesh k <murugeshk252@gmail.com>
2020-10-07 15:42:21 +0530

    modifird app.js file

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.get('/', (req, res) => {
 });
 
 // listen for requests
-app.listen(3000, () => {
+const port = process.env.port || 3000;
+app.listen(port, () => {
     console.log("Server is listening on port 3000");
 });
