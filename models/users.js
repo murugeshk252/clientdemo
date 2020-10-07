55cd1db2d0b8574b57ddeaa3170b4eb7b8a5102b
murugesh k <murugeshk252@gmail.com>
2020-10-07 15:16:38 +0530

    delete

--- models/users.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require("mongoose");
-
-var schema= mongoose.Schema;
-var  userSchema= new schema ({
-    email: String,
-    password: String,
-})
-module.exports= mongoose.model('user',userSchema,'userinfo');
